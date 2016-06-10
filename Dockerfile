f2ebb1a6917b31ead992a7379d02c3ba90d4c1a1
Navrotskyj <navrotskyj@gmail.com>
2016-06-10 15:44:28 +0300

    new Dockerfile and webitel plugins

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,14 @@ FROM node:slim
 MAINTAINER Vitaly Kovalyshyn "v.kovalyshyn@webitel.com"
 
 ENV VERSION
-ENV WEBITEL_MAJOR 3.2
+ENV WEBITEL_MAJOR 3.3
 ENV WEBITEL_REPO_BASE https://github.com/webitel
-ENV KIBANA_VERSION 4.1.2
+ENV KIBANA_VERSION 4.5.2
 
 RUN mkdir /kibana
 COPY bin /kibana/bin
 COPY src /kibana/src
+COPY optimize /kibana/optimize
 COPY config /kibana/config
 COPY plugins /kibana/plugins
 COPY LICENSE.txt /kibana/
@@ -16,8 +17,12 @@ ENV PATH /kibana/bin:$PATH
 
 ENV NODE_TLS_REJECT_UNAUTHORIZED 0
 
+RUN cd /kibana && npm install && npm cache clear && \
+    cd /kibana/installedPlugins/webitel && npm install && npm cache clear
+
 COPY ./entrypoint.sh /
 
+WORKDIR /kibana
 EXPOSE 5601
 ENTRYPOINT ["/entrypoint.sh"]
 CMD ["kibana"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,6 @@ set -e
 
 echo 'Webitel Kibana '$VERSION
 
-if [ "$SSL" ]; then
-        cp -rf 	/kibana/config/kibana_ssl.yml /kibana/config/kibana.yml
-fi
-
 # Add kibana as command if needed
 if [[ "$1" == -* ]]; then
 	set -- kibana "$@"
@@ -16,12 +12,12 @@ fi
 if [ "$1" = 'kibana' ]; then
 	if [ "$ELASTICSEARCH_URL" -o "$ELASTICSEARCH_PORT_9200_TCP" ]; then
 		: ${ELASTICSEARCH_URL:='http://elasticsearch:9200'}
-		sed -ri "s!^(elasticsearch_url:).*!\1 '$ELASTICSEARCH_URL'!" /kibana/config/kibana.yml
+		sed -ri "s!^(elasticsearch.url:).*!\1 '$ELASTICSEARCH_URL'!" /kibana/config/kibana.yml
 	fi
 
 if [ "$ENGINE_AUTH_URL" -o "$ENGINE_AUTH_PORT_10022_TCP" ]; then
 		: ${ENGINE_AUTH_URL:='http://engine:10022'}
-		sed -ri "s!^(webitel_auth:).*!\1 '$ENGINE_AUTH_URL'!" /kibana/config/kibana.yml
+		sed -ri "s!^(webitel.engineUri:).*!\1 '$ENGINE_AUTH_URL'!" /kibana/config/kibana.yml
 	fi
 
 if [ "$ENGINE_URL" -o "$ENGINE_PORT_10022_TCP" ]; then
